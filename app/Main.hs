87f538f2247607b8a73b91099bf09391f3bd5776
Samikcle <chaixiang8@gmail.com>
2024-12-19 16:49:28 +0800

    added draw maze function

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+type Maze = [[Int]]
+
+processLine :: [Int] -> String
+processLine [] = "" 
+processLine (x:xs)
+    | x == 0    = " " ++ processLine xs 
+    | x == 1    = "█" ++ processLine xs 
+    | otherwise = "?" ++ processLine xs 
+
+drawMaze :: Maze -> IO ()
+drawMaze [] = return ()
+drawMaze (x:xs) = do
+    putStrLn (processLine x)
+    drawMaze xs
+
 main :: IO ()
 main = putStrLn "Hello, Haskell Test!"
